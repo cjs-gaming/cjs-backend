a71a735c36b37cf87c4f7085494afc7135c676c5
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2020-11-11 19:31:04 -0800

    Importing

---
+++ src/main/java/com/cjsgaming/backend/CjsgamingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cjsgaming.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjsgamingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjsgamingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cjsgaming/backend/CjsgamingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjsgaming.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CjsgamingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
